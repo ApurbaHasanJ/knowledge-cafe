92bc3d84dbf26688ea7760a8fea4b9780745940d
unknown <apurbahasanj@gmail.com>
2023-04-01 00:58:56 +0600

    header responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-
-import './App.css'
-import Container from './components/Container/Container'
-import Header from './components/Header/Header'
+import './App.css';
+import Container from './components/Container/Container';
+import Header from './components/Header/Header';
 
 function App() {
   
 
   return (
-    <div className="App p-3">
+    <div className="App">
       <Header></Header>
       <Container></Container>
       
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import './Header.css'
-import User from '../../assets/User.jpg'
+import './Header.css';
+import User from '../../assets/User.jpg';
 
 const Header = () => {
-    return (
-        <div className=' mb-20'>
-            <nav className='flex justify-center items-center p-10 pb-8 border-b-4 border-indigo-300 bg-slate-100'>
-            <p className='text-4xl font-bold text-indigo-600'>Knowledge Cafe</p>
-            <div className='nav-items text-gray-900 ml-auto flex gap-4 text-xl font-medium'>
-                <a className='hover:text-indigo-600' href="#">Tutorials</a>
-                <a className='hover:text-indigo-600' href="#">Articles</a>
-                <a className='hover:text-indigo-600' href="#">Blogs</a>
-                <a className='hover:text-indigo-600' href="#">Community</a>
-                <img className='user-img rounded-full' src={User} alt="User Img" />
-            </div>
-            </nav>
-            
+  return (
+    <div className='header mb-20'>
+      <nav className='nav lg:flex justify-between items-center sm:p-2 lg:p-10 pb-8 border-b-4 border-indigo-300 bg-slate-100'>
+        <p className='logo text-4xl font-bold text-indigo-600 sm:mb-3'>Knowledge Cafe</p>
+        <div className='nav-items flex sm:flex-col lg:flex-row gap-4 text-xl font-medium items-center'>
+          <a className='hover:text-indigo-600' href='#'>
+            Tutorials
+          </a>
+          <a className='hover:text-indigo-600' href='#'>
+            Articles
+          </a>
+          <a className='hover:text-indigo-600' href='#'>
+            Blogs
+          </a>
+          <a className='hover:text-indigo-600' href='#'>
+            Community
+          </a>
+          <img className='user-img rounded-full' src={User} alt='User Img' />
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
