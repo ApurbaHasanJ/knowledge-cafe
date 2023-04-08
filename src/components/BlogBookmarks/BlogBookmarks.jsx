01734eca463f292f3e6fb6fdae8987e806d3bbac
unknown <apurbahasanj@gmail.com>
2023-04-08 14:03:24 +0600

    transfer from asignment repo

--- src/components/BlogBookmarks/BlogBookmarks.jsx
+++ src/components/BlogBookmarks/BlogBookmarks.jsx
@@ -1,47 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import './BlogBookmarks.css';
+import React, { useEffect, useState } from "react";
+import "./BlogBookmarks.css";
 
-const BlogBookmarks = ({blogReadTime, bookmarks}) => {
-    const [readTime, setReadTime] = useState(0);
-    const [bookmark, setBookmark] = useState([]);
-    
-    useEffect(() => {
-        const getBookmarkFromLS = JSON.parse(localStorage.getItem('blogBookmark'));
-        if(getBookmarkFromLS){
-            setBookmark(getBookmarkFromLS);
-        }
-        else{
-            setBookmark([]);
-        }
-    }, [bookmarks]);
+const BlogBookmarks = ({ blogReadTime, bookmarks }) => {
+  const [readTime, setReadTime] = useState(0);
+  const [bookmark, setBookmark] = useState([]);
 
-    useEffect(() => {
-        const getBlogReadTimeFromLS = localStorage.getItem('blogReadTime');
-        if (getBlogReadTimeFromLS){
-            setReadTime(getBlogReadTimeFromLS);
-        }
-        else{
-            setReadTime(0);
-        }
-    }, [blogReadTime]);
+  useEffect(() => {
+    const getBookmarkFromLS = JSON.parse(localStorage.getItem("blogBookmark"));
+    if (getBookmarkFromLS) {
+      setBookmark(getBookmarkFromLS);
+    } else {
+      setBookmark([]);
+    }
+  }, [bookmarks]);
 
-    return (
-        <div className='sticky top-7 blogBookmarks '>
-            <h1 className='mb-6 text-center font-bold border-2 border-indigo-900/50 text-indigo-700 bg-indigo-200 p-6 text-2xl rounded-lg'>
-                Spent time on read : {readTime}min
-            </h1>
-            <div className='bg-slate-200 p-7 rounded-lg'>
-                <h1 className='text-2xl font-bold'>Bookmarked Blogs : {bookmark.length}</h1>
-                {bookmark.length > 0 ? (
-                    bookmark.map((item, index) => (
-                        <p key={index} className='font-semibold text-lg mt-4 p-4 rounded-lg bg-white'>{item}</p>
-                    ))
-                ) : (
-                    <p className='font-semibold text-lg mt-4 p-4 rounded-lg bg-white'>You must set a bookmark</p>
-                )}
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const getBlogReadTimeFromLS = localStorage.getItem("blogReadTime");
+    if (getBlogReadTimeFromLS) {
+      setReadTime(getBlogReadTimeFromLS);
+    } else {
+      setReadTime(0);
+    }
+  }, [blogReadTime]);
+
+  return (
+    <div className="sticky top-7 blogBookmarks ">
+      <h1 className="mb-6 text-center font-bold border-2 border-indigo-900/50 text-indigo-700 bg-indigo-200 p-6 text-2xl rounded-lg">
+        Spent time on read : {readTime}min
+      </h1>
+      <div className="bg-slate-200 p-7 rounded-lg">
+        <h1 className="text-2xl font-bold">
+          Bookmarked Blogs : {bookmark.length}
+        </h1>
+        {bookmark.length > 0 ? (
+          bookmark.map((item, index) => (
+            <p
+              key={index}
+              className="font-semibold text-lg mt-4 p-4 rounded-lg bg-white"
+            >
+              {item}
+            </p>
+          ))
+        ) : (
+          <p className="font-semibold text-lg mt-4 p-4 rounded-lg bg-white">
+            You must set a bookmark
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default BlogBookmarks;
